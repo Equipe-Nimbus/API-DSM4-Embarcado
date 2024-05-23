2d700517216943d50ed2ca27f3fdc49670cb2abf
Matheus Melo <matheusfernando.vm@gmail.com>
2024-05-23 18:21:45 -0300

    feat(datalloger): Permitindo a função de loop aceitar mais de uma estação enviando dados para o micro serviço de recepção EN-324

--- arduino/datalloger/datalloger.ino
+++ arduino/datalloger/datalloger.ino
@@ -4,8 +4,11 @@
 
 String uidPlaca; 
 
-char *ssid = "<NOME-REDE>";
-char *pwd = "<SENHA-REDE>";
+char *ssid = "Corvinal";
+char *pwd = "senhahp126";
+
+WiFiClient wclient;
+HTTPClient http_post;
 
 char *nptServer = "br.pool.ntp.org";
 long gmtOffset = 0;
@@ -13,9 +16,10 @@ int daylight = 0;
 time_t now;
 struct tm timeinfo;
 
-String servidorRecepcao = "<IP-REDE+ROTA-SERVIÇO-APLICAÇÃO>";
+String servidorRecepcao = "http://192.168.15.8:8001/medicao/guardar";
 
 TaskHandle_t tTask1;
+TaskHandle_t tTask2;
 
 SemaphoreHandle_t mutex;
 
@@ -27,11 +31,66 @@ void minhaTask1 (void *pvPrametes) {
   Serial.println("Iniciando a Task1");
   while(true) {
     xSemaphoreTake(mutex, portMAX_DELAY);
+
+    // Alterando variáveis globais
+    uidPlaca = WiFi.macAddress();
+    uidPlaca.replace(":", "");
     temperatura = temperatura + 0.5;
     umidade = umidade + 0.5;
     bateria = bateria - 0.1;
+
+    // Requisição POST HTTP
+    http_post.begin(wclient, servidorRecepcao);
+    http_post.addHeader("Content-Type", "application/json");
+
+    String data = "{\"uuid\":\"" + uidPlaca + "\",\"unix\":" + time(&now) + ",\"bateria\":" + bateria + ",\"temperatura\":" + temperatura + ",\"umidade\":" + umidade + "}";
+
+    int http_get_status = http_post.POST(data.c_str());
+
+    Serial.println("");
+    Serial.println(http_get_status);
+    if (http_get_status == 200) {
+      Serial.println("");
+      Serial.println(data);
+      Serial.println(http_post.getString());
+    } else {
+      Serial.println("Erro ao executar o GET");
+    }
+
     xSemaphoreGive(mutex);
     delay(10000);
+  } 
+}
+
+void minhaTask2 (void *pvPrametes) {
+  Serial.println("Iniciando a Task1");
+  while(true) {
+    xSemaphoreTake(mutex, portMAX_DELAY);
+
+    // Alterando variáveis globais
+    uidPlaca = WiFi.macAddress();
+    uidPlaca.replace(":", "-");
+
+    // Requisição POST HTTP
+    http_post.begin(wclient, servidorRecepcao);
+    http_post.addHeader("Content-Type", "application/json");
+
+    String data = "{\"uuid\":\"" + uidPlaca + "\",\"unix\":" + time(&now) + ",\"bateria\":" + bateria + ",\"temperatura\":" + temperatura + ",\"umidade\":" + umidade + "}";
+
+    int http_get_status = http_post.POST(data.c_str());
+
+    Serial.println("");
+    Serial.println(http_get_status);
+    if (http_get_status == 200) {
+      Serial.println("");
+      Serial.println(data);
+      Serial.println(http_post.getString());
+    } else {
+      Serial.println("Erro ao executar o GET");
+    }
+
+    xSemaphoreGive(mutex);
+    delay(15000);
   } 
 }
 
@@ -72,6 +131,16 @@ void setup() {
       &tTask1,
       0
     );
+
+    xTaskCreatePinnedToCore(
+      minhaTask2,
+      "MinhaTask2",
+      10000,
+      NULL,
+      1,
+      &tTask2,
+      0
+    );
   }
 }
 
@@ -79,26 +148,6 @@ void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     if (!getLocalTime(&timeinfo)) {
       Serial.println("Erro ao coletar data/hora");
-    }
-
-    WiFiClient wclient;
-    HTTPClient http_post;
-
-    http_post.begin(wclient, servidorRecepcao);
-    http_post.addHeader("Content-Type", "application/json");
-
-    String data = "{\"uuid\":\"" + uidPlaca + "\",\"unix\":" + time(&now) + ",\"bateria\":" + bateria + ",\"temperatura\":" + temperatura + ",\"umidade\":" + umidade + "}";
-
-    int http_get_status = http_post.POST(data.c_str());
-
-    Serial.println("");
-    Serial.println(http_get_status);
-    if (http_get_status == 200) {
-      Serial.println("");
-      Serial.println(data);
-      Serial.println(http_post.getString());
-    } else {
-      Serial.println("Erro ao executar o GET");
     }
 
     xSemaphoreTake(mutex, portMAX_DELAY);
