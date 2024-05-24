1420e11272660c919c2941fe9da05c0c0cc9d5b8
Matheus Melo <matheusfernando.vm@gmail.com>
2024-05-24 16:43:15 -0300

    feat(datalloger): Alterando as tarefas para cada atualizar o valor global de um parâmetro em específico

--- arduino/datalloger/datalloger.ino
+++ arduino/datalloger/datalloger.ino
@@ -20,6 +20,7 @@ String servidorRecepcao = "http://192.168.15.8:8001/medicao/guardar";
 
 TaskHandle_t tTask1;
 TaskHandle_t tTask2;
+TaskHandle_t tTask3;
 
 SemaphoreHandle_t mutex;
 
@@ -36,26 +37,6 @@ void minhaTask1 (void *pvPrametes) {
     uidPlaca = WiFi.macAddress();
     uidPlaca.replace(":", "");
     temperatura = temperatura + 0.5;
-    umidade = umidade + 0.5;
-    bateria = bateria - 0.1;
-
-    // Requisição POST HTTP
-    http_post.begin(wclient, servidorRecepcao);
-    http_post.addHeader("Content-Type", "application/json");
-
-    String data = "{\"uuid\":\"" + uidPlaca + "\",\"unix\":" + time(&now) + ",\"bateria\":" + bateria + ",\"temperatura\":" + temperatura + ",\"umidade\":" + umidade + "}";
-
-    int http_get_status = http_post.POST(data.c_str());
-
-    Serial.println("");
-    Serial.println(http_get_status);
-    if (http_get_status == 200) {
-      Serial.println("");
-      Serial.println(data);
-      Serial.println(http_post.getString());
-    } else {
-      Serial.println("Erro ao executar o GET");
-    }
 
     xSemaphoreGive(mutex);
     delay(10000);
@@ -63,34 +44,30 @@ void minhaTask1 (void *pvPrametes) {
 }
 
 void minhaTask2 (void *pvPrametes) {
-  Serial.println("Iniciando a Task1");
+  Serial.println("Iniciando a Task2");
+  while(true) {
+    xSemaphoreTake(mutex, portMAX_DELAY);
+
+    // Alterando variáveis globais
+    umidade = umidade + 0.5;
+
+    xSemaphoreGive(mutex);
+    delay(15000);
+  } 
+}
+
+void minhaTask3 (void *pvPrametes) {
+  Serial.println("Iniciando a Task3");
   while(true) {
     xSemaphoreTake(mutex, portMAX_DELAY);
 
     // Alterando variáveis globais
     uidPlaca = WiFi.macAddress();
-    uidPlaca.replace(":", "-");
-
-    // Requisição POST HTTP
-    http_post.begin(wclient, servidorRecepcao);
-    http_post.addHeader("Content-Type", "application/json");
-
-    String data = "{\"uuid\":\"" + uidPlaca + "\",\"unix\":" + time(&now) + ",\"bateria\":" + bateria + ",\"temperatura\":" + temperatura + ",\"umidade\":" + umidade + "}";
-
-    int http_get_status = http_post.POST(data.c_str());
-
-    Serial.println("");
-    Serial.println(http_get_status);
-    if (http_get_status == 200) {
-      Serial.println("");
-      Serial.println(data);
-      Serial.println(http_post.getString());
-    } else {
-      Serial.println("Erro ao executar o GET");
-    }
+    uidPlaca.replace(":", "");
+    bateria = bateria - 0.1;
 
     xSemaphoreGive(mutex);
-    delay(15000);
+    delay(10000);
   } 
 }
 
@@ -141,6 +118,16 @@ void setup() {
       &tTask2,
       0
     );
+
+    xTaskCreatePinnedToCore(
+      minhaTask3,
+      "MinhaTask3",
+      10000,
+      NULL,
+      1,
+      &tTask3,
+      0
+    );
   }
 }
 
@@ -148,6 +135,23 @@ void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     if (!getLocalTime(&timeinfo)) {
       Serial.println("Erro ao coletar data/hora");
+    }
+
+    http_post.begin(wclient, servidorRecepcao);
+    http_post.addHeader("Content-Type", "application/json");
+
+    String data = "{\"uuid\":\"" + uidPlaca + "\",\"unix\":" + time(&now) + ",\"bateria\":" + bateria + ",\"temperatura\":" + temperatura + ",\"umidade\":" + umidade + "}";
+
+    int http_get_status = http_post.POST(data.c_str());
+
+    Serial.println("");
+    Serial.println(http_get_status);
+    if (http_get_status == 200) {
+      Serial.println("");
+      Serial.println(data);
+      Serial.println(http_post.getString());
+    } else {
+      Serial.println("Erro ao executar o GET");
     }
 
     xSemaphoreTake(mutex, portMAX_DELAY);
